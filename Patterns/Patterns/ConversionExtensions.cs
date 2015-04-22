79bbb1e5dd84c0067f73e24914bacfb5bd747aa1
Dejan Milicic <dejan.milicic@gmail.com>
2015-04-22 16:22:21 +0200

    Conversion extensions

---
+++ Patterns/Patterns/ConversionExtensions.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Patterns
+{
+	using System;
+	using System.Globalization;
+
+	public static class ConversionExtensions
+	{
+		public static int ToInt(this string stringValue, int defaultValue = 0)
+		{
+			if (stringValue == null) return defaultValue;
+			int integer = Int32.TryParse(stringValue, out integer) ? integer : defaultValue;
+			return integer;
+		}
+
+		public static DateTime? ToDateTime(this string dateTimeStr, string dateFmt = "yyyy-MM-ddTHH:mm:ss")
+		{
+			DateTime? result = null;
+			DateTime dt;
+			const DateTimeStyles Style = DateTimeStyles.AllowWhiteSpaces;
+			if (DateTime.TryParseExact(dateTimeStr, dateFmt, CultureInfo.InvariantCulture, Style, out dt)) result = dt;
+			return result;
+		}
+	}
+}
